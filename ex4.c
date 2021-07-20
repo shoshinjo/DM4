0e795a3592ecb78c39129507f7e0509880383e88
shoshinjo <87165681+shoshinjo@users.noreply.github.com>
2021-07-20 14:34:15 +0900

    最初のバージョン

---
+++ ex4.c
@@ -0,0 +1,21 @@
+int main(){
+int x, num, head = 0, tail = 0;
+
+printf("Tossing a coin...\n");
+
+for(x = 0; x < 3; x++){
+num = rand();
+if(num%2 == 0){
+printf("Round %d: head\n", x+1);
+head++;
+}
+
+else{
+printf("Round %d: tail\n", x+1);
+tail++;
+}
+
+printf("head: %d, tail: %d\n", head, tail);
+
+return 0;
+}
